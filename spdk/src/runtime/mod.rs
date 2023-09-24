3180da79750305acbcb6c786c122bce1a19b361e
Ed Reed <reed.ej@hotmail.com>
2023-09-24 13:55:02 -0700

    chore: reorganize runtime module

---
+++ spdk/src/runtime/mod.rs
@@ -0,0 +1,7 @@
+//! Support for Storage Performance Development Kit [Event Framework][SPEF].
+//! 
+//! [SPEF]: https://spdk.io/doc/event.html
+mod runtime;
+
+pub use runtime::Builder;
+pub use runtime::Runtime;
