0bf5722e664935f9f0832dd8333b0b4d53935085
Ed Reed <reed.ej@hotmail.com>
2025-08-13 22:51:44 -0700

    chore: adds support for rust 1.89

--- spdk-sys/src/lib.rs
+++ spdk-sys/src/lib.rs
@@ -3,15 +3,17 @@
 //!
 //! [SPDK]: https://www.spdk.io
 mod ffi {
+    #![allow(clippy::doc_lazy_continuation)]
     #![allow(clippy::missing_safety_doc)]
     #![allow(clippy::ptr_offset_with_cast)]
     #![allow(clippy::too_many_arguments)]
     #![allow(clippy::type_complexity)]
     #![allow(clippy::unnecessary_cast)]
     #![allow(clippy::useless_transmute)]
-    #![allow(non_upper_case_globals)]
     #![allow(non_camel_case_types)]
     #![allow(non_snake_case)]
+    #![allow(non_upper_case_globals)]
+    #![allow(unnecessary_transmutes)]
 
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
