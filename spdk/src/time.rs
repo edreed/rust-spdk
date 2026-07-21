860d24484a57c8d1587649f3ecd85c15b50c8b45
Ed Reed <10777868+edreed@users.noreply.github.com>
2026-07-21 11:12:54 -0700

    feat: revamps poller for safety and flexibility

--- spdk/src/time.rs
+++ spdk/src/time.rs
@@ -55,42 +55,33 @@
 //! ```
 //!
 use std::{
-    mem::MaybeUninit,
+    future::Future,
     option::Option,
-    ptr::addr_of_mut,
+    pin::Pin,
     task::{Context, Poll, Waker},
     time::{Duration, Instant},
 };
 
-use futures::future::poll_fn;
-
 use crate::task::{Polled, Poller};
 
 /// Encapsulates the execution state of an [`Interval`].
-struct IntervalInner<'a> {
-    poller: Poller<'a, Self>,
+struct IntervalInner {
     ticker: u32,
     waker: Option<Waker>,
 }
 
-impl<'a> IntervalInner<'a> {
+impl IntervalInner {
     /// Creates a new [`IntervalInner`] with a period of `period`.
-    fn new(period: Duration) -> Box<Self> {
-        let this = Box::new(MaybeUninit::<Self>::uninit());
-        let this = Box::into_raw(this) as *mut Self;
-
-        unsafe {
-            addr_of_mut!((*this).poller).write(Poller::with_period(&mut *this, period));
-            addr_of_mut!((*this).ticker).write(0);
-            addr_of_mut!((*this).waker).write(None);
-
-            Box::from_raw(this)
+    fn new() -> Self {
+        Self {
+            ticker: 0,
+            waker: None,
         }
     }
 }
 
-impl Polled for IntervalInner<'_> {
-    fn poll(&mut self) -> bool {
+impl Polled for IntervalInner {
+    fn poll(mut self: Pin<&mut Self>) -> bool {
         self.ticker += 1;
 
         if let Some(waker) = self.waker.take() {
@@ -102,32 +93,31 @@ impl Polled for IntervalInner<'_> {
     }
 }
 
+impl Future for IntervalInner {
+    type Output = Instant;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Instant> {
+        if self.ticker == 0 {
+            self.waker = Some(cx.waker().clone());
+            return Poll::Pending;
+        }
+
+        self.ticker = 0;
+
+        Poll::Ready(Instant::now())
+    }
+}
+
 /// A time interval returned by [`interval`].
 ///
 /// [`Interval`] enables you to wait asynchronously on a sequence of instants
 /// with a fixed period.
-pub struct Interval {
-    inner: Box<IntervalInner<'static>>,
-}
+pub struct Interval(Poller<IntervalInner>);
 
 impl Interval {
     /// Completes when the next instant in the interval has been reached.
-    pub async fn tick(&mut self) -> Instant {
-        let tick_fut = poll_fn(move |ctx| self.poll_tick(ctx));
-
-        tick_fut.await
-    }
-
-    /// Polls for the next instant in the interval to be reached.
-    fn poll_tick(&mut self, ctx: &mut Context<'_>) -> Poll<Instant> {
-        if self.inner.ticker == 0 {
-            self.inner.waker = Some(ctx.waker().clone());
-            return Poll::Pending;
-        }
-
-        self.inner.ticker = 0;
-
-        Poll::Ready(Instant::now())
+    pub fn tick(&mut self) -> &mut impl Future<Output = Instant> {
+        &mut self.0
     }
 }
 
@@ -141,9 +131,7 @@ impl Interval {
 ///
 /// TODO: Document drift behavior.
 pub fn interval(period: Duration) -> Interval {
-    Interval {
-        inner: IntervalInner::new(period),
-    }
+    Interval(Poller::with_period(period, IntervalInner::new()))
 }
 
 /// Waits until `duration` has elapsed.
