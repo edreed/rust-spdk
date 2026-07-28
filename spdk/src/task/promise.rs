647f3c4e674941d768405c3907eb9ff251530a41
Ed Reed <10777868+edreed@users.noreply.github.com>
2026-07-27 20:15:13 -0700

    fix: adds lifetime markers for `BdevIo<T>::allocate_buffers` operation

--- spdk/src/task/promise.rs
+++ spdk/src/task/promise.rs
@@ -34,7 +34,7 @@ use crate::{
 #[derive(Debug, Default)]
 enum PromiseState<T>
 where
-    T: Debug + 'static,
+    T: Debug,
 {
     /// The initial state of a promise.
     #[default]
@@ -72,7 +72,7 @@ unsafe impl<T> Send for PromiseState<T> where T: Debug + Send + 'static {}
 
 impl<T> PromiseState<T>
 where
-    T: Debug + 'static,
+    T: Debug,
 {
     /// Polls the state of the operation, advancing to the next state if possible.
     fn poll(&mut self, cx: &Context<'_>) -> Self {
@@ -115,7 +115,7 @@ where
 /// </div>
 pub struct Promissory<R, C = ()>
 where
-    R: Debug + 'static,
+    R: Debug,
 {
     state: RefCell<PromiseState<R>>,
     thread: Thread,
@@ -124,7 +124,7 @@ where
 
 impl<R, C> Promissory<R, C>
 where
-    R: Debug + 'static,
+    R: Debug,
     C: Unpin,
 {
     /// Returns a new `Rc<Promise>` instance with the provided context.
@@ -154,7 +154,7 @@ where
 
 impl<R, C> Promissory<R, C>
 where
-    R: Debug + 'static,
+    R: Debug,
 {
     /// Constructs a new `Rc<Promissory>` instance initializing the context, `C` in place. This
     /// function also provides a `Weak<Promissory>` to the allocation allowing you to initialize the
@@ -344,7 +344,7 @@ impl<C> Promissory<(), C> {
 
 impl<R, C> Default for Promissory<R, C>
 where
-    R: Debug + 'static,
+    R: Debug,
     C: Default,
 {
     fn default() -> Self {
@@ -359,11 +359,11 @@ where
 /// A future implementation for awaiting the result of a [`Promise`].
 struct FuturePromise<R, C>(Rc<Promissory<R, C>>)
 where
-    R: Debug + 'static;
+    R: Debug;
 
 impl<R, C> Future for FuturePromise<R, C>
 where
-    R: Debug + 'static,
+    R: Debug,
 {
     type Output = Result<R, Errno>;
 
@@ -387,13 +387,13 @@ where
 /// </div>
 pub struct Promise<R, C = ()>(Rc<Promissory<R, C>>)
 where
-    R: Debug + 'static;
+    R: Debug;
 
 unsafe impl<R, C> Send for Promise<R, C> where R: Debug + Send + 'static {}
 
 impl<R> Promise<R>
 where
-    R: Debug + 'static,
+    R: Debug,
 {
     /// Returns a new `Promise` instance.
     ///
@@ -408,7 +408,7 @@ where
 
 impl<R> Default for Promise<R>
 where
-    R: Debug + 'static,
+    R: Debug,
 {
     fn default() -> Self {
         Self::new()
@@ -417,7 +417,7 @@ where
 
 impl<R, C> Promise<R, C>
 where
-    R: Debug + 'static,
+    R: Debug,
     C: Unpin,
 {
     /// Returns a new `Promise` instance with the user context of the related `Promissory`
@@ -451,7 +451,7 @@ where
 
 impl<R, C> Promise<R, C>
 where
-    R: Debug + 'static,
+    R: Debug,
 {
     /// Constructs a new `Promise` instance initializing the context, `C`, in place in the related
     /// `Promissory` allocation. This function also provides a `Weak<Promissory>` to the allocation
