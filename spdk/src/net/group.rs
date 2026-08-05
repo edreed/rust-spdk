ec2a4a9237ab370dcf0baa14ab185f2082279e5c
Ed Reed <10777868+edreed@users.noreply.github.com>
2026-08-05 10:35:06 -0700

    doc: fix rustdoc references in net/group.rs & net/polled.rs

--- spdk/src/net/group.rs
+++ spdk/src/net/group.rs
@@ -176,6 +176,8 @@ fn stream_as_raw_sock(data: *const ()) -> *mut spdk_sock {
 ///
 /// This method returns `Poll::Ready(Ok())` if the stream is connected, `Poll::Pending` if the
 /// outgoing connection is pending, and `Poll::Ready(Err(`[`Errno`]`))` if the connection failed.
+///
+/// [`Errno`]: crate::errors::Errno
 fn stream_poll_connected(data: *const (), cx: &mut Context<'_>) -> Poll<Result<()>> {
     let this = unsafe { &mut *(data as *mut Grouped<TcpStreamSocket>) };
 
@@ -193,6 +195,8 @@ fn stream_poll_connected(data: *const (), cx: &mut Context<'_>) -> Poll<Result<(
 /// available and `Poll::Pending` if no data was available.
 ///
 /// If the connection has failed, this method returns `Poll::Ready(Err(`[`Errno`]`))`.
+///
+/// [`Errno`]: crate::errors::Errno
 fn stream_poll_read(data: *const (), cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<Result<usize>> {
     let this = unsafe { &mut *(data as *mut Grouped<TcpStreamSocket>) };
 
@@ -210,6 +214,8 @@ fn stream_poll_read(data: *const (), cx: &mut Context<'_>, buf: &mut [u8]) -> Po
 /// available and `Poll::Pending` if no data was available.
 ///
 /// If the connection has failed, this method returns `Poll::Ready(Err(`[`Errno`]`))`.
+///
+/// [`Errno`]: crate::errors::Errno
 fn stream_poll_read_vectored(
     data: *const (),
     cx: &mut Context<'_>,
@@ -231,6 +237,8 @@ fn stream_poll_read_vectored(
 /// written and `Poll::Pending` data cannot currently be written.
 ///
 /// If the connection has failed, this method returns `Poll::Ready(Err(`[``Errno`]`))`.
+///
+/// [`Errno`]: crate::errors::Errno
 fn stream_poll_write_vectored(
     data: *const (),
     cx: &mut Context<'_>,
@@ -252,6 +260,8 @@ fn stream_poll_write_vectored(
 /// written and `Poll::Pending` data cannot currently be written.
 ///
 /// If the connection has failed, this method returns `Poll::Ready(Err(`[`Errno`]`))`.
+///
+/// [`Errno`]: crate::errors::Errno
 fn stream_poll_write(data: *const (), cx: &mut Context<'_>, buf: &[u8]) -> Poll<Result<usize>> {
     let this = unsafe { &mut *(data as *mut Grouped<TcpStreamSocket>) };
 
@@ -295,6 +305,8 @@ fn stream_poll_flush(data: *const (), cx: &mut Context<'_>) -> Poll<Result<()>> 
 ///
 /// This method returns `Poll::Ready(Ok())` if the socket was successfully closed, and
 /// `Poll::Ready(Err(`[`Errno`]`))` otherwise.
+///
+/// [`Errno`]: crate::errors::Errno
 fn stream_poll_close(data: *const (), cx: &mut Context<'_>) -> Poll<Result<()>> {
     let this = unsafe { &mut *(data as *mut Grouped<TcpStreamSocket>) };
 
